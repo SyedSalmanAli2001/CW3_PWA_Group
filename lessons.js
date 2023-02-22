ca99da8625c3a2153dc9081c61941367575466ef
Syed Salman <syed.salman.ali@outlook.com>
2023-02-22 11:23:32 +0400

    added lessons.js from cw1

---
+++ lessons.js
@@ -0,0 +1,82 @@
+let subjects = [
+    {
+        "id": 01,
+        "title": "Mathematics",
+        "location": "Dubai",
+        "price": '40',
+        "image": '../images/mathematics.jpeg',
+        "spaces": 5
+    },
+    {
+        "id": 02,
+        "title": "History",
+        "location": "Dubai",
+        "price": '50',
+        "image": '../images/history.jpeg',
+        "spaces": 5
+    },
+    {
+        "id": 03,
+        "title": "Physics",
+        "location": "Sharjah",
+        "price": '30',
+        "image": '../images/physics.jpeg',
+        "spaces": 5
+    },
+    {
+        "id": 04,
+        "title": "Information Technology",
+        "location": "Ras Al-Khaima",
+        "price": '55',
+        "image": '../images/it.jpeg',
+        "spaces": 5
+    },
+    {
+        "id": 05,
+        "title": "Biology",
+        "location": "Dubai",
+        "price": '45',
+        "image": '../images/biology.jpeg',
+        "spaces": 5
+    },
+    {
+        "id": 06,
+        "title": "Chemistry",
+        "location": "Al Ain",
+        "price": '35',
+        "image": '../images/chemistry.jpeg',
+        "spaces": 5
+    },
+    {
+        "id": 07,
+        "title": "English Language",
+        "location": "Dubai",
+        "price": '60',
+        "image": '../images/english.jpeg',
+        "spaces": 5
+    },
+    {
+        "id": 08,
+        "title": "Geography",
+        "location": "Abu Dhabi",
+        "price": '25',
+        "image": '../images/geography.jpeg',
+        "spaces": 5
+    },
+    {
+        "id": 09,
+        "title": "Drama",
+        "location": "Sharjah",
+        "price": '40',
+        "image": '../images/drama.jpeg',
+        "spaces": 5
+    },
+    {
+        "id": 10,
+        "title": "Art",
+        "location": "Dubai",
+        "price": '35',
+        "image": '../images/art.webp',
+        "spaces": 5
+    }
+]
