5d8dcbdb92eeeddb3588cedb3708d306a1c596c2
Syed Salman <syed.salman.ali@outlook.com>
2023-02-22 13:31:36 +0400

    changed image sources

--- lessons.js
+++ lessons.js
@@ -4,7 +4,7 @@ let subjects = [
         "title": "Mathematics",
         "location": "Dubai",
         "price": '40',
-        "image": '../images/mathematics.jpeg',
+        "image": 'images/mathematics.jpeg',
         "spaces": 5
     },
     {
@@ -12,7 +12,7 @@ let subjects = [
         "title": "History",
         "location": "Dubai",
         "price": '50',
-        "image": '../images/history.jpeg',
+        "image": 'images/history.jpeg',
         "spaces": 5
     },
     {
@@ -20,7 +20,7 @@ let subjects = [
         "title": "Physics",
         "location": "Sharjah",
         "price": '30',
-        "image": '../images/physics.jpeg',
+        "image": 'images/physics.jpeg',
         "spaces": 5
     },
     {
@@ -28,7 +28,7 @@ let subjects = [
         "title": "Information Technology",
         "location": "Ras Al-Khaima",
         "price": '55',
-        "image": '../images/it.jpeg',
+        "image": 'images/it.jpeg',
         "spaces": 5
     },
     {
@@ -36,7 +36,7 @@ let subjects = [
         "title": "Biology",
         "location": "Dubai",
         "price": '45',
-        "image": '../images/biology.jpeg',
+        "image": 'images/biology.jpeg',
         "spaces": 5
     },
     {
@@ -44,7 +44,7 @@ let subjects = [
         "title": "Chemistry",
         "location": "Al Ain",
         "price": '35',
-        "image": '../images/chemistry.jpeg',
+        "image": 'images/chemistry.jpeg',
         "spaces": 5
     },
     {
@@ -52,7 +52,7 @@ let subjects = [
         "title": "English Language",
         "location": "Dubai",
         "price": '60',
-        "image": '../images/english.jpeg',
+        "image": 'images/english.jpeg',
         "spaces": 5
     },
     {
@@ -60,7 +60,7 @@ let subjects = [
         "title": "Geography",
         "location": "Abu Dhabi",
         "price": '25',
-        "image": '../images/geography.jpeg',
+        "image": 'images/geography.jpeg',
         "spaces": 5
     },
     {
@@ -68,7 +68,7 @@ let subjects = [
         "title": "Drama",
         "location": "Sharjah",
         "price": '40',
-        "image": '../images/drama.jpeg',
+        "image": 'images/drama.jpeg',
         "spaces": 5
     },
     {
@@ -76,7 +76,7 @@ let subjects = [
         "title": "Art",
         "location": "Dubai",
         "price": '35',
-        "image": '../images/art.webp',
+        "image": 'images/art.webp',
         "spaces": 5
     }
 ]
